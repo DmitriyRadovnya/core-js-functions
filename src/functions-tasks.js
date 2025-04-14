067012032fb7f5e8148c3e6c6f449b010149d193
DmitriyRadovnya <dima.radovnya@gmail.com>
2025-04-15 01:39:35 +0300

    fix: delete warning

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -92,16 +92,8 @@ function getPowerFunction(exponent) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom(...coefficients) {
-  if (coefficients.length === 0) {
-    return null;
-  }
-
-  return function (x) {
-    return coefficients.reduce((sum, coeff, index) => {
-      return sum + coeff * x ** (coefficients.length - 1 - index);
-    }, 0);
-  };
+function getPolynom() {
+  throw new Error('Not implemented');
 }
 
 /**
